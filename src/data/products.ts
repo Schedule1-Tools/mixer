ece956d7bd0e5a62c768c1e470985b2052165e6d
Shibkuh <SMRybka@gmail.com>
2025-07-16 12:35:57 -0400

    Update products.ts
    added ranks to products

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,32 +3,38 @@ import type { Product, ProductData } from '../types';
 export const products: Record<Product, ProductData> = {
   'OG Kush': {
     price: 35,
+    rank: '1',
     effects: ['Ca'],
     abbreviation: 'OH',
   },
   'Sour Diesel': {
     price: 35,
+    rank: '4',
     effects: ['Re'],
     abbreviation: 'SL',
   },
   'Green Crack': {
     price: 35,
+    rank: '7',
     effects: ['En'],
     abbreviation: 'GK',
   },
   'Grandaddy Purple': {
     price: 35,
+    rank: '9',
     effects: ['Se'],
     abbreviation: 'GE',
   },
   Meth: {
     price: 70,
+    rank: '6',
     effects: [],
     abbreviation: 'MH',
     addiction: 0.6,
   },
   Cocaine: {
     price: 150,
+    rank: '26',
     effects: [],
     abbreviation: 'CE',
     addiction: 0.4,
